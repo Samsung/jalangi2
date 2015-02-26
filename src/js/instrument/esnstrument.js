d6d6b70a5bd85c56b7e22aaa4971d128f32a033d
ksen <ksen@cs.berkeley.edu>
2015-02-25 18:59:29 -0800

    JSON.parse(JSON.stringify(obj)) replaced with a proper clone function.

--- src/js/instrument/esnstrument.js
+++ src/js/instrument/esnstrument.js
@@ -1086,13 +1086,77 @@ if (typeof J$ === 'undefined') {
         return node;
     }
 
+    function constructEmptyObject(o) {
+        function F() {}
+        F.prototype = o;
+        return new F();
+    }
+
+    function clone(src) { // from http://davidwalsh.name/javascript-clone
+        function mixin(dest, source, copyFunc) {
+            var name, s, i, empty = {};
+            for(name in source){
+                // the (!(name in empty) || empty[name] !== s) condition avoids copying properties in "source"
+                // inherited from Object.prototype.	 For example, if dest has a custom toString() method,
+                // don't overwrite it with the toString() method that source inherited from Object.prototype
+                s = source[name];
+                if(!(name in dest) || (dest[name] !== s && (!(name in empty) || empty[name] !== s))){
+                    dest[name] = copyFunc ? copyFunc(s) : s;
+                }
+            }
+            return dest;
+        }
+
+        if(!src || typeof src != "object" || Object.prototype.toString.call(src) === "[object Function]"){
+            // null, undefined, any non-object, or function
+            return src;	// anything
+        }
+        if(src.nodeType && "cloneNode" in src){
+            // DOM Node
+            return src.cloneNode(true); // Node
+        }
+        if(src instanceof Date){
+            // Date
+            return new Date(src.getTime());	// Date
+        }
+        if(src instanceof RegExp){
+            // RegExp
+            return new RegExp(src);   // RegExp
+        }
+        var r, i, l;
+        if(src instanceof Array){
+            // array
+            r = [];
+            for(i = 0, l = src.length; i < l; ++i){
+                if(i in src){
+                    r.push(clone(src[i]));
+                }
+            }
+            // we don't clone functions for performance reasons
+            //		}else if(d.isFunction(src)){
+            //			// function
+            //			r = function(){ return src.apply(this, arguments); };
+        }else{
+            // generic objects
+            try {
+                r = constructEmptyObject(src);
+//                r = src.constructor ? new src.constructor() : {};
+            } catch (e) {
+                console.log(src);
+                throw e;
+            }
+        }
+        return mixin(r, src, clone);
+
+    }
+
     var visitorCloneBodyPre = {
         "FunctionExpression": function (node) {
-            node.bodyOrig = JSON.parse(JSON.stringify(node.body));
+            node.bodyOrig = clone(node.body);
             return node;
         },
         "FunctionDeclaration": function (node) {
-            node.bodyOrig = JSON.parse(JSON.stringify(node.body));
+            node.bodyOrig = clone(node.body);
             return node;
         }
     };
