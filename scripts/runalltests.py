d4db0819a8f4a90f57a91c4f04d08bae403d0172
Manu Sridharan <msridhar@gmail.com>
2021-12-30 11:31:12 -0800

    Add GitHub Actions and conver test scripts to Python 3 (#172)

--- scripts/runalltests.py
+++ scripts/runalltests.py
@@ -18,7 +18,7 @@ from subprocess import call
 
 def call_fail(l):
     if call(l) != 0:
-        print "{} failed".format(" ".join(l))
+        print("{} failed".format(" ".join(l)))
         exit(1)
 
 #call_fail(["python", "scripts/units.py"])
